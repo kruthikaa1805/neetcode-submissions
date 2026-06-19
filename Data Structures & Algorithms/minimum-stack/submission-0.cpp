21620db571d05163f4fb26f104f6c3d6e6a2e9b3
kruthikaa1805 <153678836+kruthikaa1805@users.noreply.github.com>
2026-06-19 08:08:25 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,28 @@
+class MinStack {
+public:
+    MinStack() {
+        
+    }
+    stack<pair<int,int>>st;
+    
+    void push(int val) {
+        if(st.empty())
+        st.push({val,val});
+        else
+        st.push({val,min(val,st.top().second)});
+
+    }
+    
+    void pop() {
+        if(!st.empty())
+        st.pop();
+    }
+    
+    int top() {
+       return st.top().first;
+    }
+    
+    int getMin() {
+       return st.top().second;
+    }
+};

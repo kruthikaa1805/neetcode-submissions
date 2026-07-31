5730cb22b628ca86d01dd30e3123213bdf6dfb8e
kruthikaa1805 <153678836+kruthikaa1805@users.noreply.github.com>
2026-07-31 12:35:15 +0530

    Add: network-delay-time - submission-3

---
+++ Data Structures & Algorithms/network-delay-time/submission-3.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<int>dist(n+1,1e9);
+        vector<vector<pair<int,int>>>graph(n+1);
+        for(int i=0;i<times.size();i++)
+        {
+            int a=times[i][0];
+            int b=times[i][1];
+            int tim=times[i][2];
+            graph[a].push_back({b,tim});
+        }
+        dist[k]=0;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        pq.push({0,k});
+        while(!pq.empty())
+        {
+            const pair<int,int>temp=pq.top();
+            int dis=temp.first;
+            int node=temp.second;
+            pq.pop();
+            for(auto it:graph[node])
+            {
+           int adjnode=it.first;
+           int edgewt=it.second;
+                if((dis+edgewt<dist[adjnode]))
+                {
+                    
+
+                    dist[adjnode]=dis+edgewt;
+                    pq.push({dist[adjnode],adjnode});
+                }
+
+            }
+        }
+        int maxi=0;
+        for(int i=1;i<n+1;i++)
+        {
+            if(dist[i]==1e9)
+            return -1;
+            maxi=max(maxi,dist[i]);
+        }
+        return maxi;
+    }
+};

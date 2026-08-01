01e32b952212f82c7696a486ccbc78925958ff50
kruthikaa1805 <153678836+kruthikaa1805@users.noreply.github.com>
2026-08-01 12:31:46 +0530

    Add: swim-in-rising-water - submission-1

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-1.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+   vector<vector<int>>coords={{-1,0},{0,-1},{1,0},{0,1}};
+    int swimInWater(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        priority_queue<tuple<int,int,int>,vector<tuple<int,int,int>>,greater<tuple<int,int,int>>>pq;
+        pq.push({grid[0][0],0,0});
+        vector<vector<int>>visited(n,vector<int>(m,0));
+        int maxi=INT_MIN;
+        while(!pq.empty())
+        {
+            auto [time,r,c]=pq.top();
+            pq.pop();
+            visited[r][c]=1;
+            maxi=max(maxi,time);
+            if(r==n-1 && c==m-1)
+            return maxi;
+           for(int i=0;i<coords.size();i++)
+           {
+             int x=r+coords[i][0];
+             int y=c+coords[i][1];
+             if(x>=0 && x<n && y>=0 && y<m && !visited[x][y])
+             {
+              pq.push({grid[x][y],x,y});
+             }
+           }
+        }
+    return -1;
+    }
+};

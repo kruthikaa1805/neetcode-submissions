b187c7f8e160db920e171389cad9e2712cf337dd
kruthikaa1805 <153678836+kruthikaa1805@users.noreply.github.com>
2026-08-01 11:13:46 +0530

    Add: redundant-connection - submission-1

---
+++ Data Structures & Algorithms/redundant-connection/submission-1.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int findpar(int x,vector<int>&parent)
+    {
+        if(parent[x]==x)
+        return x;
+        return parent[x]=findpar(parent[x],parent);
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int n=edges.size();
+        vector<int>parent(n+1);
+        vector<int>size(n+1,1);
+        for(int i=0;i<=n;i++)
+        {
+            parent[i]=i;
+        }
+         vector<int>ans;
+        for(vector<int> &edge:edges)
+        {
+            int u=edge[0];
+            int v=edge[1];
+            int pu=findpar(u,parent);
+            int pv=findpar(v,parent);
+            if(pu==pv)
+            ans= {u,v};
+            if(size[pu]<size[pv])
+            {
+                parent[pu]=pv;
+                size[pv]+=size[pu];
+            }
+            else 
+            {
+                parent[pv]=pu;
+                size[pu]+=size[pv];
+            }
+        }
+        return ans;
+    }
+};

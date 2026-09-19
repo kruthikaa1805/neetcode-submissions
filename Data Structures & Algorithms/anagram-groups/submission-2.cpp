e91bbb8a06a73981c7b75ecd9e19316f6256c29b
kruthikaa1805 <153678836+kruthikaa1805@users.noreply.github.com>
2026-09-19 23:48:21 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>mp;
+        for(int i=0;i<strs.size();i++)
+        {
+            string temp=strs[i];
+            sort(temp.begin(),temp.end());
+            mp[temp].push_back(strs[i]);
+        }
+        vector<vector<string>>ans;
+        for(auto it:mp)
+        {
+            vector<string>temp=it.second;
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
